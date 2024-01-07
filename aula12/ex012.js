acea3a34b3d33427f796d0b6105ba36e858f4040
JefersonManso <82556932+JefersonManso@users.noreply.github.com>
2024-01-07 14:50:41 -0300

    comentário

---
+++ aula12/ex012.js
@@ -0,0 +1,10 @@
+/*var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log('Tenha um Bom dia!')
+} else if (hora <= 18) {
+    console.log('Tenha uma Ótima tarde!')
+} else {
+    console.log('Tenha uma Ótima noite!')
+}*/
