7e9ff1aa00ccb5272470ea284ec329e9c4d6bf66
JefersonManso <82556932+JefersonManso@users.noreply.github.com>
2024-01-07 20:29:44 -0300

    EXERCÍCIO 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+/*var c = 1
+do { // TESTE LÓGICO NO FINAL
+    console.log(`Passo ${c}`) 
+    c++
+} while (c <= 6) */
+
+
+
+
+/*var c = 1
+while (c <= 6) { // TESTE LÓGICO NO INÍCIO
+    console.log(`passo ${c}`)
+    c++
+}*/
